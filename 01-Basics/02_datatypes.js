8610078496456ae103b0ed5dabc2754065ae9f00
aditya <coder.programmer108@gmail.com>
2025-02-20 13:24:40 +0000

    datatypes in the js

---
+++ 01-Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let name = "RadheShyam" ;
+let num = 1 ;
+let bool = true ;
+let strange = null ;
+
+//null => standalone type ;
+//true => boolean 
+//name => string ;
+// bigint 
+//int => 2 to power 53
+
+console.log(typeof num);
