8870255165d497ad6c9e880aefbe4cdc4c0dd77a
aditya <coder.programmer108@gmail.com>
2025-03-02 14:20:54 +0000

    operator and difference between them

---
+++ 01-Basics/04_operator.js
@@ -0,0 +1,17 @@
+// ***********Easy Operator***********
+// let a=2 ;
+// let b=2 ;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+// ********some tricy**********
+console.log(null > 0 );
+console.log("000" >= 0);
+console.log(null >= 0);
+console.log(undefined >= 0);
+
+// '===' this operator checks the datatypes as well while equality check
+
+
