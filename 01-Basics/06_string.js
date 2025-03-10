70abe1ea666dc8285c4d65f8cb90808fc7845161
aditya <coder.programmer108@gmail.com>
2025-03-10 21:11:05 +0000

    basics of the string

---
+++ 01-Basics/06_string.js
@@ -0,0 +1,13 @@
+let name = "chintu" ;
+console.log(name);
+let age = 50 ;
+// console.log("My name is ", name, " and my age is ", age); this is the old tarika 
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+let getName = "Jindagi jiyo aarmse padhai karo dam lagake aaur mast raho aapne saath mai :)" ;
+
+console.log(getName.__proto__);
+console.log(Object.getPrototypeOf(getName)); //this are the two functions to do this same task used it with the wisely
+
+console.log(getName.toUpperCase());
