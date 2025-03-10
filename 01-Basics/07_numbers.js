86b5ac4b358e07fbaa8477b077a039b7d52ac40e
aditya <coder.programmer108@gmail.com>
2025-03-10 21:31:44 +0000

    number study

---
+++ 01-Basics/07_numbers.js
@@ -0,0 +1,7 @@
+let age = 50 ;         //this is the number itself without any object property
+
+console.log(age);
+
+let salary = new Number(1000.2500); //this is making the object explicitly 
+console.log(salary);
+
