fbf88b0d86ad93938dabc7a487979701ca628558
aditya <coder.programmer108@gmail.com>
2025-03-11 11:44:29 +0000

    strings operations and the imprtance

--- 01-Basics/06_string.js
+++ 01-Basics/06_string.js
@@ -5,9 +5,15 @@ let age = 50 ;
 
 console.log(`My name is ${name} and my age is ${age}`);
 
-let getName = "Jindagi jiyo aarmse padhai karo dam lagake aaur mast raho aapne saath mai :)" ;
+let getName = new String("     Jindagi jiyo aarmse padhai karo dam lagake aaur mast raho aapne saath mai :)   ") ;
 
 console.log(getName.__proto__);
 console.log(Object.getPrototypeOf(getName)); //this are the two functions to do this same task used it with the wisely
 
 console.log(getName.toUpperCase());
+
+console.log(getName.slice(1,4)) ; //slicing is important 
+console.log(getName.trim());
+ 
+console.log(getName.charAt(5));
+
