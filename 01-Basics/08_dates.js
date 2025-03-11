0cab14ac604ca0a3bb59d03ef0a17b9971747d66
aditya <coder.programmer108@gmail.com>
2025-03-11 13:44:16 +0000

    depth dates

--- 01-Basics/08_dates.js
+++ 01-Basics/08_dates.js
@@ -11,8 +11,32 @@ console.log(Object.getPrototypeOf(myDate));
 // console.log(myDate.toLocaleString(), "This is the string of the date object string , this is the actual format we uses :)");
 // console.log(myDate.toTimeString());
 
-let createdDate = new Date(2023,1,1);
-console.log(createdDate);
+let createdDate = new Date(2023,2,1,5,30,32);
+
+console.log(createdDate.toLocaleString()); 
+
+let generateDate = new Date("2023-12-23");
+console.log(generateDate.toLocaleString(), "This date has the pattern like MM-DD-YY");
+
+let myTimeStamp = Date.now() ;
+
+console.log(Math.floor(myTimeStamp/1000)); //this will convert it into the total number of the seconds 
+
+let newDate = new Date();
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1); //because the month strt from the 0 in the js 
+console.log(newDate.getMinutes());
+console.log(newDate.getSeconds());
+
+console.log(newDate.toLocaleString('default', {
+    weekday : "long",
+    minute : "2-digit",
+    day : "2-digit",
+})); //this is the customization of the date 
+
+
+// console.log(typeof myDate);
 
 
 
